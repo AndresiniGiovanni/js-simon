288ef8bd4ab0357f10d7b1b109c34833fc5872ba
AndresiniGiovanni <andresini.giovanni@gmail.com>
2022-10-19 18:47:45 +0200

    non funzionante

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 // Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 5 secondi.
 // Dopo 30 secondi i numeri scompaiono e l'utente deve inserire,
 // uno alla volta, i numeri che ha visto precedentemente, tramite una casella di input e un bottone
@@ -12,44 +11,61 @@
 let divNum = document.getElementById("numeri");
 let numeriutente = document.getElementById("usernum");
 let button = document.getElementById("btnEsegui");
-let numeri = [];
+let risultato = document.getElementById("risultato");
+let ArrayRandom = [];
+let arrayUser = [];
 let numeroDiNumeri = 5;
-let insertNumber = [];
 
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-//mi metto 5 dentro una variabile;
+//mi metto 5 numeri dentro una variabile;
 //uso un ciclo while per generare i 5 numeri e popolare l'array;
 //finché numeri.length < 5 continua a girare;
 // genero un numero random dentro al while ;
 //
 //prendo il div dall'html e assegno una variabile;
-//ciclo for per metterli nel div stampando nell'html;
-while (numeri.length < numeroDiNumeri) {
+while (ArrayRandom.length < numeroDiNumeri) {
   let numero = randomNumber(1, 100);
-  if (!numeri.includes(numero)) {
-    //controllo numeri.includes. if not numeri.includes ;
-    numeri.push(numero); //allora numeripush
+  if (!ArrayRandom.includes(numero)) {
+    //controllo numeri.includes. if not numeri.includes
+    ArrayRandom.push(numero); //allora numeripush
   }
 }
-divNum.innerText = numeri;
+divNum.innerText = ArrayRandom;
 
 //chiedere i numeri all'utente
 //inserisco un input type text nell'hmtl
-//funzione che controlla i numeri inseriti dall'utente
+
 //dopo 5 volte dico quanti numeri ha indovinato
 
 function clear() {
   divNum.innerText = "";
 }
 
-setTimeout(clear, 5000);  // function setTimeout di 5 secondi /5000ms/
-function insert() {
-  if (insertNumber.length == numeri.length) {  //function nascondere i numeri dopo 5 sec
-  } else {
-    insertNumber.push(numeriutente.value);
+setTimeout(clear, 3000); // function setTimeout di 3 secondi /3000ms/ function nascondere i numeri dopo 3 sec
+
+function inserisci() {
+  arrayUser.push(numeriutente);
+  let valore = parseInt(arrayUser.value);
+  arrayUser.push(valore);
+  console.log(arrayUser);
+  arrayUser.value = "";
+}
+
+//funzione che controlla i numeri inseriti dall'utente
+function controllo() {
+  let win;
+  for (let i = 0; i < numeroDiNumeri; i++) {
+    if (arrayUser[i] !== ArrayRandom[i]) {
+      win = false;
+      risultato.innerText = `hai perso`;
+    } else {
+      win = true;
+      risultato.innerText = `hai vinto`;
+    }
+    console.log(win);
   }
 }
-console.log(numeri);
-button.addEventListener("click", insert);
+
+console.log(ArrayRandom);
+console.log(numeriutente);
+console.log(arrayUser);
+button.addEventListener("click", controllo);
